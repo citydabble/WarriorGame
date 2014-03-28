b425c1799f194e15b93faf966b0197b9448dd211
Hayden Nhan <hdnhan@gmail.com>
2014-03-27 23:00:51 -0700

    Initial Commit

---
+++ WarriorGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WarriorGame
+//
+//  Created by Hayden on 2014-03-27.
+//  Copyright (c) 2014 Hayden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
